83cc93cd0caf8bc330f0766e859d19fcd685b338
anandrajeshmishra <anand98425@gmail.com>
2025-06-02 16:11:18 +0530

    work

--- DAY1.java
+++ DAY1.java
@@ -1,5 +1,53 @@
+// class DAY1{
+//     public static void main(String[] args){
+//         // System.out.println("Hello world with java");
+//         // System.out.print("Hello world with java\nfrom anandkumar mishra");
+//         // System.out.print("*\n**\n***\n****");
+//         String name = "Anandkumar Mishra";
+//         int age = 20;
+//         double price = 25.25;
+//         int a = 25, b = 10;
+//         System.out.println(name);
+//     }
+// }
+// import java.util.*;
+// class DAY1{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.next();
+//         System.out.println(name);
+//     }
+// }
+// import java.util.*;
+// class DAY1{
+//     public static void main(String [] args){
+//         System.out.print("Enter your name here: ");
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.next();
+//         System.out.println(name);
+//     }
+// }
+// import java.util.*;
+// class DAY1{
+//     system.out.print("Enter Your name here: ");
+//     Scanner sc = new Scanner(System.in);
+//     String name = sc.next();
+//     System.out.println(name);
+// }
+// import java.util.*;
+// class DAY1{
+//     public static void main(String [] args){
+//         System.out.print("Enter your name here");
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.next();
+//         System.out.println(name);
+//     }
+// }
+import java.util.*;
 class DAY1{
-    public static void main(String args[]){
-        System.out.println("Hello World");
+    public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println(name);
     }
 }
--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
 class hello{
-
-    public static void main(String args[]) {
-        System.out.println("Hello World");
+    public static void main(String args[]){
+        System.out.println("hello here my name is anandkumar mishre and I am going to be the one of the best codders in the world");
     }
 }
