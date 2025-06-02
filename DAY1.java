133d67aaa318adc9e571949992df9ad39325c545
anandrajeshmishra <anand98425@gmail.com>
2025-06-02 16:30:34 +0530

    work

--- DAY1.java
+++ DAY1.java
@@ -45,9 +45,11 @@
 // }
 import java.util.*;
 class DAY1{
-    public static void main(String [] args){
-    Scanner sc = new Scanner(System.in);
-    String name = sc.next();
-    System.out.println(name);
+    public static void main(String[] args){
+        System.out.println("Enter your name here");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is :");
+        System.out.println(name);
     }
 }
