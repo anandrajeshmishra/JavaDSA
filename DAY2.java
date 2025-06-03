e45cd214fa7c28012d455378ca0f558e3cacbc1c
anandrajeshmishra <anand98425@gmail.com>
2025-06-03 14:43:27 +0530

    work

--- DAY2.java
+++ DAY2.java
@@ -1,17 +1,31 @@
 import java.util.*;
 class DAY2{
     public static void main(String [] args){
-        System.out.println("Enter your age here");
+        System.out.println("Enter two numbers to campare");
+        // System.out.print("Enter a number to check odd or even");
+        // System.out.println("Enter your age here");
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if(age>=18){
-            System.out.println("Adult");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // int num = sc.nextInt();
+        if(a==b){
+            System.out.println("equal");
+        }else if(a>b){
+            System.out.println("a is greater");
         }else{
-            System.out.println("not an Adult");
+            System.out.println("a is lesser");
         }
+        // if(num%2==0){
+            // System.out.println("Adult");
+            // System.out.println("even");
+        // }else{
+            // System.out.println("not Adult");
+            // System.out.println("odd");
+        // }
         // int a = sc.nextInt();
         // int b = sc.nextInt();
         // int sum = a+b;
         // System.out.println(sum);
+        
     }
 }
