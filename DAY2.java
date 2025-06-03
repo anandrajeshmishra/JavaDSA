1ea8f792bdcb2fead6c756fc01180765c46de9ad
anandrajeshmishra <anand98425@gmail.com>
2025-06-03 14:55:53 +0530

    work

--- DAY2.java
+++ DAY2.java
@@ -1,20 +1,31 @@
 import java.util.*;
 class DAY2{
     public static void main(String [] args){
-        System.out.println("Enter two numbers to campare");
+        System.out.println("Enter numbers between 1 2 3");
+        // System.out.println("Enter two numbers to campare");
         // System.out.print("Enter a number to check odd or even");
         // System.out.println("Enter your age here");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        // int num = sc.nextInt();
-        if(a==b){
-            System.out.println("equal");
-        }else if(a>b){
-            System.out.println("a is greater");
+        int button = sc.nextInt();
+        if(button == 1){
+            System.out.println("Hello");
+        }else if(button == 2){
+            System.out.println("Namaste");
+        }else if (button == 3){
+            System.out.println("Bonjour");
         }else{
-            System.out.println("a is lesser");
+            System.out.println("Bhai kya bol raha hai");
         }
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int num = sc.nextInt();
+        // if(a==b){
+        //     System.out.println("equal");
+        // }else if(a>b){
+        //     System.out.println("a is greater");
+        // }else{
+        //     System.out.println("a is lesser");
+        // }
         // if(num%2==0){
             // System.out.println("Adult");
             // System.out.println("even");
