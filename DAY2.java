c4e6337d5513fe9ea6f2aa541f58662c9290544e
anandrajeshmishra <anand98425@gmail.com>
2025-06-03 15:17:14 +0530

    work

--- DAY2.java
+++ DAY2.java
@@ -1,21 +1,27 @@
 import java.util.*;
 class DAY2{
     public static void main(String [] args){
-        System.out.println("Enter numbers between 1 2 3");
+
+
+        // if else switch break
+
+
+
+        // System.out.println("Enter numbers between 1 2 3");
         // System.out.println("Enter two numbers to campare");
         // System.out.print("Enter a number to check odd or even");
         // System.out.println("Enter your age here");
-        Scanner sc = new Scanner(System.in);
-        int button = sc.nextInt();
-        switch(button){
-            case 1: System.out.println("hello");
-            break;
-            case 2: System.out.println("namaste");
-            break;
-            case 3: System.out.println("bonjour");
-            break;
-            default: System.out.println("Bhai kya bol raha hai");
-        }
+        // Scanner sc = new Scanner(System.in);
+        // int button = sc.nextInt();
+        // switch(button){
+        //     case 1: System.out.println("hello");
+        //     break;
+        //     case 2: System.out.println("namaste");
+        //     break;
+        //     case 3: System.out.println("bonjour");
+        //     break;
+        //     default: System.out.println("Bhai kya bol raha hai");
+        // }
         // if(button == 1){
         //     System.out.println("Hello");
         // }else if(button == 2){
@@ -46,6 +52,17 @@ class DAY2{
         // int b = sc.nextInt();
         // int sum = a+b;
         // System.out.println(sum);
+
+
+        // Loops
         
+
+        // for(int i = 0; i<100; i++){
+        //     System.out.print("hello world");
+        // }
+        int num = 11;
+        for(int i = 0; i<num; i++){
+            System.out.println(i);
+        }
     }
 }
