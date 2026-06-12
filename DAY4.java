41f1421d56ff4aed5e94efa5f8fac34d60431fbd
anandrajeshmishra <anand98425@gmail.com>
2026-06-12 23:02:29 +0530

    work

---
+++ DAY4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class DAY4{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        // code to find the Adult 
+        // System.out.print("Enter your age: ");
+        // int age = sc.nextInt();
+        // if(age>=18){
+        //     System.out.println("Adult");
+        // }else{
+        //     System.out.println("Not Adult");
+        // }
+        // code to find out the number is even or odd
+        System.out.print("Enter the number to check odd or even: ");
+        int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+
+    }
+}
